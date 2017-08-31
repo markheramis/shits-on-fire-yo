aaeb8144474d014e12805dabddf4ca07033f1cdc
Mark Clu Heramis <chumheramis@gmail.com>
2017-09-01 00:31:00 +0800

    forgot to push all branch yo
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- exec-commit-all.sh
+++ exec-commit-all.sh
@@ -1,3 +1,3 @@
 for d in ~/projects/ ; 
-do (cd "$d" && git add . && git commit -sm "Shits on fire yo, I'm out.");
+do (cd "$d" && git add . && git commit -sm "Shits on fire yo, I'm out." && git push --all && git push --tags);
 done
